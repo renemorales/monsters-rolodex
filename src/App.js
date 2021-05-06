1a9076934e9f1d187f81e4996b0fb489437e0357
Rene Morales <renemorales.developer@gmail.com>
2021-05-06 16:41:47 -0500

    added a search components and then I created a search feature that takes a users types input and filter out the card base on what the user types in

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
+import { CardList } from './components/card-list/card-list.component';
+import { SearchBox } from './components/search-box/search.component';
+
 import './App.css';
 
 class App extends Component {
@@ -7,30 +10,29 @@ class App extends Component {
     super(); // access to this.state
 
     this.state = { // state is some object with property that can access at anytime
-      monsters:[
-        {
-          name: 'Frankenstein',
-          id: 'asc1'
-        },
-        {
-          name: 'Dracula',
-          id: 'asr2'
-        },
-        {
-          name: 'Zombie',
-          id: 'as1w'
-        }
-      ]
+      monsters:[],
+      searchField: ''
     };
   }
 
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => this.setState({ monsters: users }));
+  }
+
   render(){
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter(monster =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+      );
     return (
       <div className="App">
-       {
-         this.state.monsters.map(monster => (
-         <h1 key={monster.id}> { monster.name} </h1>
-          ))}
+        <SearchBox 
+          placeholder= 'search monsters'
+          handleChange = {e => this.setState({ searchField: e.target.value })}
+        />
+        <CardList monsters={ filteredMonsters } />
       </div>
     );
   }
